08e21d75ce815b7de1f02f83d8ab99c99a5bd1b7
Chuck Owuzo <ifyowuzo@gmail.com>
2023-01-09 13:49:21 -0500

    added prac.py file

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # python_practice
 mudheromedia
+ifeanyi 
+
---
+++ prac.py
@@ -0,0 +1,10 @@
+# comment goes here:
+
+# Data Types
+
+# 1. String
+name = "ifeanyi"
+
+print(name.upper())
+print(name.isupper())
+print(name.find("y"))
