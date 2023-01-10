fa826ad218e6fb4452a71c8c4a983f97db50f818
Chuck Owuzo <ifyowuzo@gmail.com>
2023-01-10 14:27:39 -0500

    make some more changes

---
+++ prac1.py
@@ -0,0 +1,51 @@
+# comment goes here:
+
+# Data Types
+
+# 1. String
+name = "ifeanyi"
+
+print(name.upper())
+print(name.isupper())
+print(name.find("y"))
+
+statement = "my namE is Bukola"
+
+print(statement.capitalize())
+
+print(statement.split())
+
+print(statement.swapcase())
+
+print("OMOLEWA")
+statement = "i am here."
+
+
+
+
+name = "big boy"
+print(name.upper())
+print(statement.upper())
+print(statement.count(name))
+print(name.swapcase())
+
+statement = "2+2=4"
+print(statement)
+print(statement.isupper())
+print(statement.replace(name,statement,3))
+print(name.replace(statement,statement,4))
+
+print(slice.__text_signature__)
+numbers = 42
+
+# print(numbers)
+#
+# statement = "i am here"
+# print(statement.capitalize())
+# print(vars(dict))
+
+print(type(statement))
+
+
+
+
---
+++ prac2.py
@@ -0,0 +1,5 @@
+my_name = "Omolewa Bukola Ify Adaramola"
+
+date = "11/02/2023"
+
+print(date.split('/'))
